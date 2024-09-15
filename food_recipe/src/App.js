4fae71ff15840bb4cfc16da18ec5d5c7ec20125a
Lav <lavrentibasilashvili@gmial.com>
2024-09-15 19:54:39 +0400

    Food recipe API

---
+++ food_recipe/src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Recipe from './Recipe';
+
+const App = () => {
+
+  const APP_ID = process.env.REACT_APP_API_ID;
+  const APP_KEY = process.env.REACT_APP_API_KEY;
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setRecipes(data.hits);
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+    }
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.uri} 
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
